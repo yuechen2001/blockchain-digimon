2901fbf22cc253dcea9d058470ace3f3c41547e6
Zhu Yuechen <zhuyuechen@gmail.com>
2025-04-10 18:59:25 -0700

    Revert prisma file to not set database url in env

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,16 @@
 import { PrismaClient } from '@prisma/client';
-import dotenv from 'dotenv';
 
-// Load environment variables
-dotenv.config();
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+// Learn more: https://pris.ly/d/help/next-js-best-practices
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 const isProduction = process.env.DEPLOY_ENV === 'production';
 
-// Set DATABASE_URL if not already set
-if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = isProduction 
-    ? process.env.POSTGRES_DATABASE_URL 
-    : process.env.SQLITE_DATABASE_URL;
-}
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: isProduction ? [] : ['query'],
-});
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: isProduction ? [] : ['query'],
+  });
 
 if (!isProduction) globalForPrisma.prisma = prisma;
